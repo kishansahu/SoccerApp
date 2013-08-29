f6d437b5c0e81dbfdbe960ee861ef7bb2100f950
Anuj Chhabra <anujchhabra@qainfotech.net>
2013-08-29 18:18:37 +0530

    commiting code for timeline

---
+++ SoccerApp/SoccerTablet/liveClipsSoccer/assets/js/vendor/newCanvas.js
@@ -0,0 +1,368 @@
+jQuery.fn.DrawCanvas = function (myJsonVariable) /* call : $('#test').DrawCanvas(); */
+{
+	$.extend(myJsonVariable);
+	return this.each (function () 
+	{
+	
+		//alert();
+		c=document.getElementById(this.id);
+		ctx=c.getContext("2d");
+		c.addEventListener("click", onCanvasClick, false);
+
+		var PointCollection = [];
+		var maxWidth;
+		var pixelPerMinute,pixelPerTimeGap,pixelLoop;
+		var time,timeGap; 
+	
+		//ball images
+		ballImg = new Image();
+		ballImg.src = 'ball.png';
+		arrowImg = new Image();
+		arrowImg.src = '/arrows.png';	
+		
+		// Set field parameter
+		setFieldParameter();
+		
+		// Set field parameter
+		setFieldParameter();
+		
+		//Positioning Field
+		drawField();
+	
+		// Positioning elapsed Time
+		drawTimeLine(time);
+		// To set the field parameter
+		function setFieldParameter()
+		{
+			time = myJsonVariable.TimeElasped; 
+			timeGap = myJsonVariable.TimeGap != ""?myJsonVariable.TimeGap : 5;
+			maxWidth = c.width - 10;
+			if(time < 90)
+			{
+				pixelPerMinute = maxWidth/90;
+				pixelLoop = Math.floor(90/timeGap)+1;
+			}
+			else
+			{
+				pixelPerMinute = maxWidth/((90 + timeGap) + (Math.floor((time-90)/timeGap)*timeGap));
+				pixelLoop = Math.floor(time/timeGap)+1;
+			}
+			pixelPerTimeGap = pixelPerMinute * timeGap;
+			
+			// positioning cards.
+			var redCardForTopTeam = myJsonVariable.TeamTopRedCard;
+			var redCardForBottomTeam = myJsonVariable.TeamBottomRedCard;
+			var yellowCardForTopTeam = myJsonVariable.TeamTopYellowCard;
+			var yellowCardForBottomTeam = myJsonVariable.TeamBottomYellowCard;
+			
+			setCard(redCardForTopTeam,'Red',true);
+			setCard(redCardForBottomTeam,'Red',false);
+			
+			setCard(yellowCardForTopTeam,'Yellow',true);
+			setCard(yellowCardForBottomTeam,'Yellow',false);
+			
+			// positioning goals.
+			var goalForTopTeam = myJsonVariable.TeamTopGoal;
+			var goalForBottomTeam = myJsonVariable.TeamBottomGoal;
+			
+			setGoal(goalForTopTeam,true);
+			setGoal(goalForBottomTeam,false);
+			
+			// positioning replacement.
+			var replacementForTopTeam = myJsonVariable.TeamTopReplacement;
+			var replacementForBottomTeam = myJsonVariable.TeamTopReplacement;
+			
+			setReplacement(replacementForTopTeam,true);
+			setReplacement(replacementForBottomTeam,false);
+		}
+		
+		// To draw elapsed time line
+		function drawTimeLine(time)
+		{
+			var timeElapsed;
+			timeElapsed = pixelPerMinute*time;
+			//ctx.clearRect(0,10,timeElapsed-35,50);
+			ctx.fillStyle = "rgba(255, 255, 255, 0.25)";//"yellow";
+			ctx.fillRect(timeElapsed,10,630,80); // botx,topx,boty,topy
+			ctx.fill();
+		}
+
+		// To draw field
+		function drawField()
+		{
+			// Green Area
+			var botx=0, boty=c.height, topx=0, topy=0, bx=pixelPerTimeGap, tx=pixelPerTimeGap;
+			var color = ["#6AA828","#559412"];//6AA828,559412
+			var text=0;
+			for(var i=0;i<=pixelLoop;i++)
+			{
+				
+				ctx.fillStyle = color[i%2];//"#33CC33";//"#164C19";
+				ctx.fillRect(botx,30,botx+pixelPerTimeGap,40); // botx,topx,boty,topy // (0,70,638,40
+				ctx.fill();
+				drawLine(botx,boty-30);
+				
+				if(i>0)
+					{
+				 ctx.font = "12px Helvetica, sans-serif";
+					ctx.strokeStyle="white";
+					ctx.fillStyle = "white";
+					ctx.strokeText(text===0?timeGap:text, botx-9, 65);
+					if(text===0)
+						text+=parseInt(timeGap);
+					text+=parseInt(timeGap);
+					}
+				botx += bx;
+					
+			}
+			
+		}
+		
+		// To draw time line
+		function drawLine(x,y)
+		{
+			ctx.beginPath();
+			ctx.moveTo(x,30);//ctx.moveTo(349,0);
+			ctx.lineTo(x,y);//ctx.lineTo(356,70);
+			// ctx.closePath();
+			ctx.lineWidth=1;
+			ctx.strokeStyle='#929C93';//"#00459C";
+			ctx.closePath();
+			ctx.stroke();
+			ctx.strokeStyle=null;
+		}
+		
+		// To set the card
+		function setCard(cardList,color,top)
+		{
+			if(cardList != "")
+			{		
+				var List = cardList.split("|"); 
+				var card = [];
+				for(var i=0;i<List.length;i++)
+				{
+					var str = List[i].split(",");
+					card[i] = [str[0],str[1]];
+				}
+				// Positioning Cards
+				drawCard(card,color,top); 
+			}
+		}
+		
+		// To set the goal
+		function setGoal(goalList,top)
+		{
+			if(goalList != "")
+			{		
+				var List = goalList.split("|"); 
+				var goal = [];
+				for(var i=0;i<List.length;i++)
+				{
+					var str = List[i].split(",");
+					goal[i] = [str[0],str[1]];
+				}
+				// Positioning Goals
+				drawGoal(goal,top); 
+			}
+		}
+		
+		// To set the replacement
+		function setReplacement(repalcementList,top)
+		{
+			if(repalcementList != "")
+			{		
+				var List = repalcementList.split("|"); 
+				var replacement = [];
+				for(var i=0;i<List.length;i++)
+				{
+					var str = List[i].split(",");
+					replacement[i] = [str[0],str[1]];
+				}
+				// Position Replacements
+				drawReplacement(replacement,top); 
+			}
+		}
+		
+		// To set the card position
+		function drawCard(Card,color,team)
+		{
+			
+			if(Card.length > 0)
+			{
+				var timeElapsed;
+				ctx.fillStyle=color;
+				for(var i=0; i< Card.length; i++)
+				{
+					timeElapsed = Card[i][0]*pixelPerMinute;
+					if(team === true)
+					{
+						ctx.fillRect(timeElapsed-4,15,8,15); // botx,topx,boty,topy
+						PointCollection.push({
+							message: Card[i][1],
+							width: 8,
+							height: 15,
+							x: timeElapsed-4,
+							y: 15,
+							isAbove:!team,
+							isBelow:team
+						});
+					}
+					else
+					{
+						ctx.fillRect(timeElapsed-4,70,8,15);
+						PointCollection.push({
+							message: Card[i][1],
+							width: 8,
+							height: 15,
+							x: timeElapsed-4,
+							y: 70,
+							isAbove:!team,
+							isBelow:team
+						});
+					}
+					ctx.fill();
+				}
+			}
+		}
+
+		// To set the goal position
+		function drawGoal(Goal,team)
+		{
+			if(Goal.length > 0)
+			{
+				var timeElapsed;
+				for(var i=0; i< Goal.length; i++)
+				{
+					ctx.beginPath();
+					ctx.fillStyle = 'red';
+					timeElapsed = Goal[i][0]*pixelPerMinute;
+					if( team === true)
+					{
+					   // ctx.arc(timeElapsed+10, 25, 5, 0, 2 * Math.PI, false);
+						ctx.drawImage(ballImg, timeElapsed-8, 15,15,15);
+						PointCollection.push({
+							message: Goal[i][1],//'Left foot shot from outside the D circle',
+							width: 10,
+							height: 10,
+							x: timeElapsed-8, 
+							y: 15,   
+							isAbove:!team,
+							isBelow:team
+						});
+					}
+					else
+					{
+						//ctx.arc(timeElapsed+10, 75, 5, 0, 2 * Math.PI, false);
+						ctx.drawImage(ballImg, timeElapsed-8, 70,15,15);
+						PointCollection.push({
+							message: Goal[i][1],
+							width: 10,
+							height: 10,
+							x: timeElapsed-8,
+							y: 70, 
+							isAbove:!team,
+							isBelow:team
+						});
+					}
+					ctx.fill();
+					ctx.stroke();
+				}
+			}
+		}
+		
+		// To set the replacement position
+		function drawReplacement(Replacement,team)
+		{
+			if(Replacement.length > 0)
+			{
+				var timeElapsed;
+				for(var i=0; i< Replacement.length; i++)
+				{
+					ctx.beginPath();
+					ctx.fillStyle = 'red';
+					timeElapsed = Replacement[i][0]*pixelPerMinute;
+					if( team === true)
+					{
+					   // ctx.arc(timeElapsed+10, 25, 5, 0, 2 * Math.PI, false);
+						ctx.drawImage(arrowImg, timeElapsed-4, 15,15,15);
+						PointCollection.push({
+							message: Replacement[i][1],
+							width: 15,
+							height: 15,
+							x: timeElapsed-4, 
+							y: 15,   
+							isAbove:!team,
+							isBelow:team
+						});
+					}
+					else
+					{
+						//ctx.arc(timeElapsed+10, 75, 5, 0, 2 * Math.PI, false);
+						ctx.drawImage(arrowImg, timeElapsed-4, 70,15,15);
+						PointCollection.push({
+							message: Replacement[i][1],
+							width: 15,
+							height: 15,
+							x: timeElapsed-4,
+							y: 70, 
+							isAbove:!team,
+							isBelow:team
+						});
+					}
+					ctx.fill();
+					ctx.stroke();
+				}
+			}
+		}
+		
+		// To handle click event
+		function onCanvasClick(e) 
+		{
+			if(e.offsetX && e.offsetY)
+			{
+				var x=e.offsetX;
+				var y=e.offsetY;
+			}
+			else
+			{
+				var x=e.layerX;
+				var y=e.layerY;
+			}
+			for(i=0;i<PointCollection.length;i++)
+			{
+			   if( x >= PointCollection[i].x && x <= (PointCollection[i].x+PointCollection[i].width) &&   
+				   y >= PointCollection[i].y && y<=(PointCollection[i].y+PointCollection[i].height))
+			   {
+				   /*$(".canvas-message")
+					   .text(PointCollection[i].message)
+					   .css("top",y-50+"px")
+					   .css("left",x+10+"px")
+					   .show(); */
+				   $(".canvas-message").text(PointCollection[i].message);
+				   var messageDiv=$(".canvas-message").show().get()[0];
+				   var messageDivHeight=messageDiv.offsetHeight;
+				   var messageDivWidth=messageDiv.offsetWidth;
+				   if(PointCollection[i].isAbove)
+				   {
+					   $(".canvas-message")
+						   .css("top",PointCollection[i].y - messageDivHeight -5  +"px")
+						   .css("left",PointCollection[i].x - (messageDivWidth/2) +"px");
+				   }
+				   else
+				   {
+					   $(".canvas-message")
+						   .css("top", PointCollection[i].y + PointCollection[i].height + 5 +"px")
+						   .css("left",PointCollection[i].x - (messageDivWidth/2) + "px");
+
+				   }
+				   break;
+			   }
+			   else
+			   {
+				   $(".canvas-message").hide();
+			   }
+		   }
+	   }
+	});
+}
+
--- SoccerApp/SoccerTablet/liveClipsSoccer/src/com/liveclips/soccer/activity/AppLaunchActivity.java
+++ SoccerApp/SoccerTablet/liveClipsSoccer/src/com/liveclips/soccer/activity/AppLaunchActivity.java
@@ -2,10 +2,13 @@ package com.liveclips.soccer.activity;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.liveclips.soccer.R;
 import com.liveclips.soccer.database.LCSQLiteHelper;
@@ -13,27 +16,56 @@ import com.liveclips.soccer.database.LCSQLiteHelper;
 public class AppLaunchActivity extends Activity {
 
 	ActionBar actionBar;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		actionBar = getActionBar();
-		actionBar.hide();
-		
+		/**
+		 * Create application database if it don't exists
+		 */
 		SQLiteDatabase sqLiteDatabase = new LCSQLiteHelper(this)
 				.getReadableDatabase();
 		sqLiteDatabase.close();
+
 		setContentView(R.layout.app_launch_page);
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				final Intent mainIntent = new Intent(AppLaunchActivity.this,
-						SignUpOptionsActivity.class);
-				AppLaunchActivity.this.startActivity(mainIntent);
-				AppLaunchActivity.this.finish();
+		getActionBar().hide();
+		/**
+		 * Start the Sign Up options activity for user to login application.
+		 */
+		/*
+		 * new Handler().postDelayed(new Runnable() {
+		 * 
+		 * @Override public void run() { final Intent mainIntent = new
+		 * Intent(AppLaunchActivity.this, SignUpOptionsActivity.class);
+		 * AppLaunchActivity.this.startActivity(mainIntent);
+		 * AppLaunchActivity.this.finish(); } }, 3000);
+		 */
+		final WebView w = (WebView) findViewById(R.id.playCardBackSideTimeLineWebView);
+		w.getSettings().setLoadWithOverviewMode(true);
+		w.getSettings().setUseWideViewPort(true);
+		WebSettings webSettings = w.getSettings();
+		webSettings.setBuiltInZoomControls(true);
+		w.setWebChromeClient(new WebChromeClient());
+		webSettings.setLoadsImagesAutomatically(true);
+		webSettings.setPluginsEnabled(true);
+
+		webSettings.setJavaScriptEnabled(true);
+		w.loadUrl("file:///android_asset/handle.html");
+
+		w.setWebViewClient(new WebViewClient() {
+
+			public void onPageFinished(WebView view, String url) {
+				Log.e("intent value", "start");
+
+				loadValueToPage(w);
+
 			}
-		}, 3000);
+		});
 
 	}
+
+	private void loadValueToPage(WebView w) {
+		w.loadUrl("javascript:loadValueToPage('Liver Pool','Aston Villa','75','18.00,Red card message|25,second message','35,first message|30.00,second message','23,mn','45,mn','34,bv','42,kj','45,mn','60,mn');executeCanvas();");
+	}
+
 }
